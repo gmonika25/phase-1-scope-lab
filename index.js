e8cbc06fc561e3446f9ecddfd5b1969b053f6e09
Monika Gustilo <monikagustilo@gmail.com>
2022-07-05 21:16:03 -0700

    commit tree 3d980bd91c602f25c7828084279a529c0a614e82

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+// console.log(setBestCustomer());
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "bobbie";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bobby";
+}
